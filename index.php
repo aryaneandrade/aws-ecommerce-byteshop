f18f4087f24256d26f2d6de09c2a3c38383dca81
Aryane <aryaneands@gmail.com>
2025-11-24 18:18:56 -0300

    atualizando a home

--- index.php
+++ index.php
@@ -12,9 +12,6 @@ include_once("templates/header.php");
         <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
             <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                 <li class="nav-item">
-                    <a class="nav-link active fw-bold" aria-current="page" href="#">Home</a>
-                </li>
-                <li class="nav-item">
                     <a class="nav-link fw-bold" href="#categorias">Categorias</a>
                 </li>
                 <li class="nav-item">
